fa6a59771eb08ff7fcb651486c0dd42b9f0b6959
annaBohlin <anna@bohlinoffice.se>
2023-04-23 12:26:51 +0200

    added edit title functionality

--- frontend/frontend.js
+++ frontend/frontend.js
@@ -22,11 +22,19 @@ getAllListItems();
 const createItem = async (item) => {
   let li = document.createElement("li");
   li.id = item.id;
-  li.innerHTML = `${item.title}`;
+
+  let titleSpan = document.createElement("span");
+  titleSpan.setAttribute("id", `title-span-${item.id}`);
+  titleSpan.textContent = item.title;
 
   let statusCheckbox = document.createElement("input");
   statusCheckbox.setAttribute("type", "checkbox");
   statusCheckbox.setAttribute("id", `checkbox-${item.id}`);
+
+  let editButton = document.createElement("button");
+  editButton.setAttribute("content", "test content");
+  editButton.textContent = "Edit";
+  editButton.setAttribute("id", `edit-btn-${item.id}`);
 
   let deleteButton = document.createElement("button");
   deleteButton.setAttribute("content", "test content");
@@ -34,8 +42,7 @@ const createItem = async (item) => {
   deleteButton.setAttribute("id", `delete-btn-${item.id}`);
 
   ul.appendChild(li);
-  li.appendChild(statusCheckbox);
-  li.appendChild(deleteButton);
+  li.append(titleSpan, statusCheckbox, editButton, deleteButton);
 
   statusCheckbox.addEventListener("change", async function (e) {
     e.preventDefault();
@@ -44,6 +51,15 @@ const createItem = async (item) => {
     const checkboxId = e.target.id;
 
     await editStatus(itemId, checkboxId);
+  });
+
+  editButton.addEventListener("click", async function (e) {
+    e.preventDefault();
+
+    const itemId = e.target.parentNode.id;
+    const editButtonId = e.target.id;
+
+    await openEditTitle(itemId, editButtonId);
   });
 
   deleteButton.addEventListener("click", async function (e) {
@@ -86,6 +102,63 @@ form.addEventListener("submit", async function (e) {
   taskInput.value = "";
 });
 
+const openEditTitle = async (itemId) => {
+  console.log("openEditTitle() called for item with id:", itemId);
+
+  let task = document.getElementById(itemId);
+  let titleElement = document.getElementById(`title-span-${itemId}`);
+  let titleText = titleElement.innerText;
+
+  let editInput = document.createElement("input");
+  editInput.setAttribute("type", "text");
+  editInput.setAttribute("id", `edit-input-${itemId}`);
+  editInput.defaultValue = titleText;
+
+  let editSubmitButton = document.createElement("button");
+  editSubmitButton.setAttribute("content", "test content");
+  editSubmitButton.textContent = "Confirm";
+  editSubmitButton.setAttribute("id", `edit-submit-btn-${itemId}`);
+
+  task.append(editInput, editSubmitButton);
+
+  editSubmitButton.addEventListener("click", async function (e) {
+    e.preventDefault();
+
+    const editSubmitButtonId = e.target.id;
+
+    await submitEditedTitle(itemId, editSubmitButtonId);
+  });
+};
+
+const submitEditedTitle = async (itemId) => {
+  console.log("submitEditedTitle() called for item with id:", itemId);
+
+  let editInput = document.getElementById(`edit-input-${itemId}`);
+  let editedTitle = editInput.value;
+  let editSubmitButton = document.getElementById(`edit-submit-btn-${itemId}`);
+
+  editSubmitButton.remove();
+  editInput.remove();
+
+  const response = await fetch(`/task/${itemId}/title`, {
+    method: "PATCH",
+    headers: {
+      "Content-Type": "application/json; charset=UTF-8",
+    },
+    body: JSON.stringify({ title: editedTitle }),
+  });
+
+  let data = await response.json();
+  console.log("data:", data);
+
+  const titleElement = document.getElementById(`title-span-${itemId}`);
+  console.log(titleElement);
+
+  titleElement.textContent = data.title;
+
+  console.log("response:", response);
+};
+
 const editStatus = async (itemId, checkboxId) => {
   console.log("editStatus() called for item with id:", itemId);
 
@@ -93,7 +166,7 @@ const editStatus = async (itemId, checkboxId) => {
 
   const completed = statusCheckbox.checked;
 
-  const response = await fetch(`/task/${itemId}`, {
+  const response = await fetch(`/task/${itemId}/status`, {
     method: "PATCH",
     headers: {
       "Content-Type": "application/json; charset=UTF-8",
