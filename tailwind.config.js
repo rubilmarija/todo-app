0c931ce4eec72140cd595cf780d65e5637b0212b
Munniza <you@example.com>
2023-04-28 11:32:30 +0200

    latest changes

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: [
+    "./frontend/frontend.js",
+    "./frontend/index.html",
+    "./**/*.{html, js}",
+  ],
   theme: {
     extend: {},
   },
   plugins: [],
-}
-
+};
